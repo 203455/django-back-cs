37ac78113717348c325aa9d080a1764f309f32db
203455 <77992738+203455@users.noreply.github.com>
2022-01-23 13:53:34 -0600

    create primerComponente and integrate model

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #componentes 
+    'primerComponente',
+    
     # librerias agregadas al proyecto
     'rest_framework',
+    
+    
 ]
 
 REST_FRAMEWORK = {
