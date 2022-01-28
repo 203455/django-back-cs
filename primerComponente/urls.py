cf284dbd52b55b465789efdb0cfae828a21ac220
203455 <77992738+203455@users.noreply.github.com>
2022-01-27 21:26:47 -0600

    Add views class service

--- primerComponente/urls.py
+++ primerComponente/urls.py
@@ -4,9 +4,11 @@ from django.conf.urls import include
 
 #importamos view
 from primerComponente.views import PrimerTablaList
+from primerComponente.views import PrimerTablaDetails
 
 urlpatterns = [
     re_path(r'^lista/$', PrimerTablaList.as_view()),
+    re_path(r'^lista/(?P<pk>\d+)$', PrimerTablaDetails.as_view()),
 ]
 
 
